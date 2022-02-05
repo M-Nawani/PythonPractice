70183ec34877ab7d1c1a97c4492cdf9f23349c9b
M-Nawani <57030407+M-Nawani@users.noreply.github.com>
2022-02-05 16:40:03 +0530

    Update main.py
    Implementing Exception Handling

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -5,9 +5,16 @@ data_set = pandas.read_csv("nato_phonetic_alphabet.csv")
 # using comprehension
 data_dict = {row.letter: row.code for (index, row) in data_set.iterrows()}
 
-name = input("Enter your name: ").upper()
-final_list = [data_dict[letter] for letter in name]
-print(final_list)
+
+def generate_phonetic():
+    name = input("Enter your name: ").upper()
+    try:
+        final_list = [data_dict[letter] for letter in name]
+    except KeyError:
+        print('Sorry only alphabets allowed in the name')
+        generate_phonetic()
+    else:
+        print(final_list)
 
 
-
+generate_phonetic()
